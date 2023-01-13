f4b29a424d5676a262735ca1649ca76af0fb4add
EmilianoCBE <e.carrizales1305@gmail.com>
2023-01-13 15:31:03 -0600

    First Router added

--- src/HeroesApp.jsx
+++ src/HeroesApp.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import AppRouter from './router/AppRouter'
 
 const HeroesApp = () => {
   return (
     <>
-        <h1>Heroes App</h1>
+        <AppRouter />
     </>
   )
 }
