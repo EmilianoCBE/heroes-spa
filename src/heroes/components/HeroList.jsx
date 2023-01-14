29ab8e0fc0d3c751f03b6d3ce397d977e26f5a4d
EmilianoCBE <e.carrizales1305@gmail.com>
2023-01-13 19:15:49 -0600

    Character cards added

---
+++ src/heroes/components/HeroList.jsx
@@ -0,0 +1,19 @@
+import { getHeroesByPublisher } from '../helpers/getHeroesByPublisher'
+import HeroCard from './HeroCard'
+
+const HeroList = ({publisher}) => {
+
+    const heroes = getHeroesByPublisher(publisher)
+
+  return (
+    <div className="row row-cols-1 row-cols-md-3 g-4">
+        {
+            heroes.map(hero => 
+                <HeroCard key={hero.id} {...hero}/>
+            )
+        }
+    </div>
+  )
+}
+
+export default HeroList
